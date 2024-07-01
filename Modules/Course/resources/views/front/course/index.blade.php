46e362b7a6043e875c16ce65dc3218458bc8c5a8
arshia <arshiabat5@gmail.com>
2024-07-01 22:04:26 +0430

    site map

--- Modules/Course/resources/views/front/course/index.blade.php
+++ Modules/Course/resources/views/front/course/index.blade.php
@@ -82,7 +82,7 @@
                                                 </div>
                                             </div>
                                             <div class="col-md-7">
-                                                <div class="card-body">
+                                                <div class="card-body pb-0">
                                                     <!-- Badge and rating -->
                                                     <div
                                                         class="d-flex justify-content-between align-items-center mb-2"
