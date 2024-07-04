ad9f2f3f03d433d882f89ec19fce80e1892bef19
arshia <arshiabat5@gmail.com>
2024-07-04 23:26:24 +0430

    bootcamp

--- Modules/Course/App/Http/Controllers/Front/CourseController.php
+++ Modules/Course/App/Http/Controllers/Front/CourseController.php
@@ -31,7 +31,7 @@ class CourseController extends Controller
     public function show($show)
     {
         $course = Course::where('slug',$show)->first();
-        if($course){
+        if(!empty($course['items'])){
             abort(404);
         }
         if ($course->status ==0){
