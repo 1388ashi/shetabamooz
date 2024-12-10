b487a7557799fca914827221cf9688fc721f7119
1388ashi <arshiabat5@gmail.com>
2024-12-10 17:15:17 +0330

    modify DBcontroller

--- app/Http/Controllers/DBController.php
+++ app/Http/Controllers/DBController.php
@@ -7,16 +7,17 @@ use Illuminate\Database\Schema\Blueprint;
 class DBController extends Controller
 {
     public function add(){
-        // Schema::create('bootcamp_comments', function (Blueprint $table) {
-        //     $table->id();
-        //     $table->foreignId('bootcamp_id')->constrained('bootcamps')->cascadeOnDelete();
-        //     $table->string('name');
-        //     $table->string('mobile');
-        //     $table->string('description');
-        //     $table->enum('status', ['pending','rejected','accepted']);
-        //     $table->timestamps();
-        // });
-        // dd('ok');
+        Schema::create('bootcamp_comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('bootcamp_id')->constrained('bootcamps')->cascadeOnDelete();
+            $table->string('name');
+            $table->string('mobile');
+            $table->string('description');
+            $table->string('admin_description')->nullable();
+            $table->enum('status', ['pending','rejected','accepted']);
+            $table->timestamps();
+        });
+        dd('ok');
         // Schema::table('bootcamp_comments', function (Blueprint $table) {
         //         $table->string('admin_description')->nullable();
         // });
