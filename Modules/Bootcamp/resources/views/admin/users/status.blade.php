98f6fd7e1d13e16b2f517b6f5ff476def9d71430
1388ashi <arshiabat5@gmail.com>
2024-12-12 10:50:39 +0330

    modify sms bootcamp users

--- Modules/Bootcamp/resources/views/admin/users/status.blade.php
+++ Modules/Bootcamp/resources/views/admin/users/status.blade.php
@@ -1,9 +1,7 @@
 @if($user->status == 'new')
 <span title="وضعیت" class="badge badge-primary ">جدید</span>
-@else
 @elseif($user->status == 'absent')
 <span title="وضعیت" class="badge badge-success ">غایب</span>
-@else
 @elseif($user->status == 'present')
 <span title="وضعیت" class="badge badge-success ">حاضر</span>
 @else
